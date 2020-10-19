dd94750c447ccb1ba726fd5045196bf78ec319b2
cartgo <43975440+cartgo@users.noreply.github.com>
2020-10-18 22:54:21 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,196 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import MyTable from './Table/MyTable';
+import axios from 'axios'; 
+class App extends Component {
+
+  state = {
+     posts:[],
+    postId: null,
+    pointer : null,
+    linepicker:null,
+    id :'',
+    createitem : false
+ };
+
+ delete(postsIndex){
+
+  const posts = [...this.state.posts];
+  posts.splice(postsIndex, 1);
+  this.setState({posts:posts})
+ }
+
+
+create(){
+  this.setState({createitem:true})
+
+axios.post( 'https://jsonplaceholder.typicode.com/posts' , {
+        userId: 'new user id',
+        title : 'new title',
+        body : 'new body'
+      })
+      .then( res => {
+        console .log(res);
+        let newPost = res.data;
+        newPost.id =this.state.id;
+        let newData = [...this.state.posts, newPost];
+        this .setState({
+          posts : newData,
+          id: this.state.id +1
+        });
+      })
+      .catch( err => console .log(err));
+      
+}
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+      const posts = res.data;
+
+      this.setState({ posts:posts,
+      id: posts[posts.length - 1].id+1 
+     });
+   
+    })  }
+ 
+ 
+
+
+  keydown = (event,index) =>{
+    if(event.key === 'Enter'){
+      this.setState({
+        pointer:null,linepicker:null
+    });
+    
+    }
+  }
+
+
+// click(index){
+//   this.setState({pointer:index})
+// }
+
+click(index,name){
+  this.setState({pointer:name,
+  linepicker:index})
+}
+ 
+handleChange(event,index,name){
+ console.log(event.target.value)
+ const posts = [...this.state.posts];
+ const post = posts[index];
+ if(name == 'userId' ){
+  console.log('userId')
+  if(event.target.value >0){
+  post.userId = Math.round(event.target.value)}
+ }else if(name =='title'){console.log('title')
+ post.title = event.target.value;}
+ else{console.log('body')
+ post.body = event.target.value;
+
+ }
+ }
+
+
+
+  render() {     
+
+    let postinfo = null;
+ 
+ 
+postinfo = 
+<ul>
+<table>
+    <thead>
+
+            <tr>
+              <th>ID</th>
+              <th>USER ID</th>
+              <th>TITLE</th>
+              <th>BODY</th>
+              <th>OPERATION</th>
+            </tr>
+    </thead>
+    <tbody>
+  { this.state.posts.map((post,index) => {
+    if(this.state.linepicker!=index){
+      return       <tr> 
+    <td className = 'idCo'>{post.id}</td>
+    <td className = 'userIdCo' onClick = {()=>this.click(index,'userId')}>{post.userId}&nbsp;</td>
+    <td className = 'titleCo' onClick = {()=>this.click(index,'title')}>{post.title}&nbsp;</td>
+    <td className = 'bodyCo'  onClick = {()=>this.click(index,'body')}>{post.body}&nbsp;</td>
+    <td className = 'buttonCo'> <button onClick =  {()=>this.delete(index)}>delete</button></td>
+    </tr>}
+    else{
+    switch(this.state.pointer){
+      case 'userId':
+          return <tr> 
+                  <td className = 'idCo'>{post.id}</td>
+                  <td><label>userId:
+                      <input type="number" name="userId" 
+                      onChange={(event,name)=>this.handleChange(event,index,name = 'userId')}
+                      onKeyDown = {(event) => {this.keydown(event,index )}}/>
+                  </label></td>
+                  <td className = 'titleCo' onClick = {()=>this.click(index,'title')}>{post.title}&nbsp;</td>
+                  <td className = 'bodyCo'  onClick = {()=>this.click(index,'body')}>{post.body}&nbsp;</td>
+                  <td className = 'buttonCo'> <button onClick =  {()=>this.delete(index)}>delete</button></td>
+                  </tr>
+       case 'title':
+        return <tr> 
+                  <td className = 'idCo'>{post.id}</td>
+                  <td className = 'userIdCo' onClick = {()=>this.click(index,'userId')}>{post.userId}&nbsp;</td>
+                  <td><label>title:
+                      <input type="text" name="title" 
+                      onChange={(event,name)=>this.handleChange(event,index,name = 'title')}
+                      onKeyDown = {(event) => {this.keydown(event,index )}}/>
+                  </label></td>
+                  <td className = 'bodyCo'  onClick = {()=>this.click(index,'body')}>{post.body}&nbsp;</td>
+                  <td className = 'buttonCo'> <button onClick =  {()=>this.delete(index)}>delete</button></td>
+                  </tr>
+         case 'body':
+            return <tr> 
+                  <td className = 'idCo'>{post.id}</td>
+                  <td className = 'userIdCo' onClick = {()=>this.click(index,'userId')}>{post.userId}&nbsp;</td>
+                  <td className = 'titleCo' onClick = {()=>this.click(index,'title')}>{post.title}&nbsp;</td>
+                  <td><label>body:
+                        <input type="text" name="body" 
+                        // value={this.state.posts[index].userId} 
+                        onChange={(event,name)=>this.handleChange(event,index,name = 'body')}
+                        onKeyDown = {(event) => {this.keydown(event,index )}}/>
+                  </label></td>
+                  <td className = 'buttonCo'> <button onClick =  {()=>this.delete(index)}>delete</button></td>
+                  </tr>
+    }}    
+  }  
+    )
+    }
+  <br/>&nbsp; <br/><br/>
+  </tbody>
+
+  </table>
+</ul>
+ 
+ 
+
+     return (
+      <div className = 'App-header'>
+ 
+    
+        {postinfo}
+      <button onClick = {()=>this.create()}>add item</button>
+              
+
+
+
+
+      </div>
+    )
+ 
+   
+
+
+}
+}
+export default App;
---
+++ src/Table/MyTable.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// // import logo from './logo.svg';
+// // import './App.css';
+
+// const MyTable = (props) => {
+//     return (
+//         <tr> 
+//                   <td className = 'idCo'>{props.id}</td>
+//                   <td><label>userId:
+//                       <input type="number" name="userId" 
+//                       onChange={(event,name)=>this.handleChange(event,index,name = 'userId')}
+//                       onKeyDown = {(event) => {this.keydown(event,index )}}/>
+//                   </label></td>
+//                   <td className = 'titleCo' onClick = {()=>this.click(index,'title')}>{props.title}&nbsp;</td>
+//                   <td className = 'bodyCo'  onClick = {()=>this.click(index,'body')}>{props.body}&nbsp;</td>
+//                   <td className = 'buttonCo'> <button onClick =  {()=>this.delete(index)}>delete</button></td>
+//                   </tr>
+
+//     )
+
+
+// }
+
+// export default MyTable;
